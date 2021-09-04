f029fa8d4046b4c2ae4d0d1edbf189ce92483b17
Ali Karkazan <ali.karkazan@icloud.com>
2021-09-04 06:08:22 +0400

    already solved practice up to 03_09

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,11 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const myObject = {
+  name: "Ali",
+  age: 36,
+  Hieght: 177,
+  Weaight: 126,
+};
+
+console.log(myObject);
--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,10 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  changeValume: function (newValume) {
+    this.volume = newValume;
+  },
 };
